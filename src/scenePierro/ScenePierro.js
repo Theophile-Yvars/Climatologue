2bb29b6fa9d663dde2bf23bf6aa6d18a3b407e3f
julesprince <jules.prince.fr@icloud.com>
2023-12-08 07:41:31 +0100

    finaml

--- src/scenePierro/ScenePierro.js
+++ src/scenePierro/ScenePierro.js
@@ -33,6 +33,10 @@ function ScenePierro() {
   const [usineAlreadyShown, setUsineAlreadyShown] = useState(false);
   const [arbreAlreadyShown, setArbreAlreadyShown] = useState(false);
 
+  function test(b) {
+    setIsVisible(b)
+  }
+
   
   function playCar(e ) {
     const clickAudio = new Audio(klaxon);
@@ -44,7 +48,8 @@ function ScenePierro() {
     
     if(!carAlreadyShown)
       setIndiceLeft(indiceLeft-1);
-    setCarAlreadyShown(true)
+    setCarAlreadyShown(true);
+    
   }
   
   function playArbre(e ) {
@@ -71,7 +76,8 @@ function ScenePierro() {
 
     if(!cochonAlreadyShown)
       setIndiceLeft(indiceLeft-1);
-    setCochonAlreadyShown(true)
+    setCochonAlreadyShown(true);
+  
   }
   
   function playUsine(e ) {
@@ -85,6 +91,7 @@ function ScenePierro() {
     if(!usineAlreadyShown)
       setIndiceLeft(indiceLeft-1);
     setUsineAlreadyShown(true)
+  
   }
   
   function playClimatosceptique(e ) {
@@ -106,13 +113,22 @@ function ScenePierro() {
       const randomAudio = new Audio(audioFiles[randomIndex]);
       randomAudio.play();
   }
+
+    function testLimit(){
+      console.log(indiceLeft)
+      if(indiceLeft===0) {
+        window.location.href = '/sceneGeorge';
+      }
+    }
+
+
   
     
 
     return (
       <>
 
-        <div >
+        <div style={{ display: 'none' }}>
       <H5AudioPlayer
       autoPlayAfterSrcChange={true}
         autoPlay={true}
@@ -136,10 +152,12 @@ function ScenePierro() {
           <p>Vous avez face à vous un climatosceptique qui soutient que le réchauffement climatique n'existe pas.</p>
           <p>Pour lui le climat à toujours changé.</p>
           <p>Trouvez les 4 causes du réchauffement climatique pour lui démontrer son erreur</p>
-          <button onClick={setIsVisible(false)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Play !</button>
+          <button onClick={() => test(false)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Play !</button>
         </div>
-      ) }
-{/*
+      )}
+        
+
+        
 {showDivCar && (
         <div className="carExplained" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '999', backgroundColor: 'rgba(255, 255, 255, 0.9)', padding: '20px', borderRadius: '10px' }}>
         <h1>Impact du Transport sur le Climat</h1>
@@ -147,7 +165,8 @@ function ScenePierro() {
   <p>De plus, le transport de marchandises à grande échelle, notamment par voie maritime, génère également des émissions considérables. Les activités de transport ont un impact non seulement sur la qualité de l'air, mais aussi sur le réchauffement global de la planète, ce qui nécessite des efforts pour développer des solutions durables et moins polluantes.</p>
   <p>La transition vers des modes de transport plus respectueux de l'environnement, tels que les véhicules électriques, les transports en commun efficaces, et la réduction des distances parcourues, sont des éléments cruciaux pour atténuer l'impact négatif du transport sur le climat.</p>
   
-        <button onClick={setShowDivCar(false)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
+        <button onClick={() => {setShowDivCar(false);
+        testLimit();}} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
         </div>
       )}
 
@@ -158,7 +177,8 @@ function ScenePierro() {
   <p>Les arbres absorbent le dioxyde de carbone (CO2) de l'atmosphère et stockent ce carbone, aidant ainsi à réguler le climat. La déforestation libère ce carbone stocké dans les arbres sous forme de CO2, contribuant ainsi à l'augmentation des gaz à effet de serre dans l'atmosphère.</p>
   <p>En plus de libérer du CO2, la déforestation conduit à la perte de biodiversité, à la destruction des habitats naturels pour de nombreuses espèces animales et végétales, ainsi qu'à une augmentation de l'érosion des sols et des risques d'inondations.</p>
   <p>Pour atténuer les effets néfastes de la déforestation, il est crucial de promouvoir la reforestation, de préserver les forêts existantes, de développer des pratiques agricoles durables et de promouvoir une exploitation forestière responsable.</p>
-        <button onClick={setShowDivArbre(false)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
+        <button onClick={() => {setShowDivArbre(false);
+        testLimit();}} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
         </div>
       )}
       {showDivUsine && (
@@ -168,7 +188,8 @@ function ScenePierro() {
   <p>Ces émissions massives contribuent à l'effet de serre, piégeant la chaleur du soleil et provoquant le réchauffement climatique. Les conséquences de ce phénomène incluent la fonte des calottes glaciaires, l'élévation du niveau des mers, l'augmentation des événements météorologiques extrêmes tels que les tempêtes et les sécheresses, ainsi que des perturbations graves dans les écosystèmes naturels.</p>
   <p>En outre, l'industrialisation intensive a également entraîné une déforestation massive pour répondre aux besoins en matières premières et à l'expansion des zones industrielles. Cette déforestation contribue à la perte de biodiversité, à l'érosion des sols et à la libération de dioxyde de carbone stocké dans les arbres, amplifiant ainsi le changement climatique.</p>
   <p>Il est crucial de prendre des mesures significatives pour réduire les émissions industrielles, investir dans des technologies propres et durables, ainsi qu'adopter des pratiques respectueuses de l'environnement afin de limiter les effets néfastes de l'industrialisation sur notre climat et notre planète.</p>
-        <button onClick={setShowDivUsine(false)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
+        <button onClick={() => {setShowDivUsine(false);
+        testLimit();}} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
         </div>
       )}
       {showDivCochon && (
@@ -178,10 +199,12 @@ function ScenePierro() {
   <p>Les animaux d'élevage, tels que les vaches et les moutons, produisent du méthane, un gaz à effet de serre beaucoup plus puissant que le dioxyde de carbone (CO2). Les déchets animaux et la gestion des déchets dans les élevages contribuent également aux émissions de gaz à effet de serre, aggravant ainsi le problème du réchauffement climatique.</p>
   <p>En plus des émissions de gaz, l'élevage intensif nécessite d'importantes ressources en eau, en alimentation pour les animaux, ainsi que des terres agricoles pour leur élevage et leur nourriture. Cette demande accrue en ressources conduit à la déforestation, à la perte de biodiversité et à une utilisation excessive des terres, exacerbant les problèmes environnementaux.</p>
   <p>Il est essentiel d'explorer des méthodes d'élevage plus durables et respectueuses de l'environnement, telles que l'agriculture biologique, l'élevage en pâturage et la réduction de la consommation de produits issus de l'élevage intensif, afin de réduire l'impact néfaste de l'élevage sur le climat et l'environnement.</p>
-        <button onClick={setShowDivCochon(false)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
+        <button onClick={() => {setShowDivCochon(false);
+        testLimit();}} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>Close</button>
         </div>
-      )}*/}
-      
+      )}
+
+
       </>
       
     );
