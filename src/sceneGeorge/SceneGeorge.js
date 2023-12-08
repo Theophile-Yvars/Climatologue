2b0536aeb6256896d207ab9a358c62bc254e7a8d
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2023-12-08 02:59:40 +0100

    diag

--- src/sceneGeorge/SceneGeorge.js
+++ src/sceneGeorge/SceneGeorge.js
@@ -3,9 +3,13 @@ import './SceneGeorge.css'
 
 function SceneGeorge() {
     return (
-        <div className="SceneGeorge">
-            <img src={imgSceneGeorge} alt="scenePierro"/>
-        </div>
+        <>
+            <div className="SceneGeorge">
+                <img src={imgSceneGeorge} alt="scenePierro"/>
+            </div>
+
+            <div className="SceneGeorge-Dialogue"></div>
+        </>
     );
 }
 
