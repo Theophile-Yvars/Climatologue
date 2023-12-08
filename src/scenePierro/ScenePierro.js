472f9bbb73e2f22fa7271f304c53e13152042001
julesprince <jules.prince.fr@icloud.com>
2023-12-08 02:58:53 +0100

    clickable responsive div

--- src/scenePierro/ScenePierro.js
+++ src/scenePierro/ScenePierro.js
@@ -2,10 +2,17 @@
 import imgScenePierro from "../res/img/scenePierro.png"
 import "./scenePierro.css"
 
+function handleClick(e, area1) {
+  console.log("click on : " + area1)
+}
+
 function ScenePierro() {
     return (
       <div className="ScenePierro">
         <img src={imgScenePierro} alt="scenePierro" />
+        <div className="clickable-area" style={{ top: '18%', left: '10%', width: '18%', height: '10%' }} onClick={(e) => handleClick(e, 'Voiture')} > </div>
+        <div className="clickable-area" style={{ top: '29%', left: '33%', width: '11%', height: '10%' }} onClick={(e) => handleClick(e, 'Voiture')} > </div>
+        <div className="clickable-area" style={{ top: '50%', left: '60%', width: '15%', height: '5%' }} onClick={(e) => handleClick(e, 'Area 2')} />
       </div>
     );
   }
